29f4c4e05d38220cffa0af4317358f8837d1491f
Argenthands <alejandro.david.benolol@gmail.com>
2022-05-02 14:54:32 +0300

    starter version done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-document.getElementById("player").addEventListener("click",sumarPuntos)
-puntos = 0;
+document.getElementById("player1").addEventListener("mouseover",sumarPuntos)
+let puntos = 0;
+let tiempo = 5
+
 function sumarPuntos(){
     puntos++
-    document.getElementById("puntos").innerHTML = puntos
+    document.getElementById("puntos").innerHTML ="<b>" + puntos + "</b>";
+    randomNumberTop = (Math.floor(Math.random() * (70 - 1)) + 1);
+    randomNumberLeft = (Math.floor(Math.random() * (90 - 1)) + 1);
+    document.getElementById("player1").style.marginTop = randomNumberTop + "vh";
+    document.getElementById("player1").style.marginLeft = randomNumberLeft + "vh";
 }
+
+function restarTiempo(){
+    if(tiempo > 0){
+        tiempo --
+    }
+    document.getElementById("tiempo").innerHTML ="<b>" + tiempo + "</b>";
+    if (tiempo == 0){
+        document.getElementById("player1").removeEventListener("mouseover",sumarPuntos);
+        document.getElementById("player1").innerHTML = "<h1>GAME OVER</h1>";
+        document.getElementById("player1").style.background = "none";
+        document.getElementById("player1").style.border = "none";
+    }
+}
+
+setInterval(restarTiempo,1000);
